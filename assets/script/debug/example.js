f5bb55217b1ab2e42dfdf0cbc3784654866d830a
jungyeon0708 <kimkjy0708@gmail.com>
2025-06-18 10:35:58 +0900

    [학습] eslint 설치 및 실습

--- assets/script/debug/example.js
+++ assets/script/debug/example.js
@@ -1,5 +1,21 @@
 const button = document.querySelector(".button");
+console.log(button);
+// button.addEventListener("click", (e) => {
+//   console.log("버튼 클릭!");
+// });
 
-button.addEventListener("click", (e) => {
+let condition = true; // 조건을 true로 설정
+if (condition) {
+  console.log("참입니다!");
+} else {
+  console.log("거짓입니다!");
+}
+
+let link;
+
+link = document.querySelector(".link");
+console.log(link);
+
+button.addEventListener("click", () => {
   console.log("버튼 클릭!");
 });
