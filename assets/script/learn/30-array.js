46836041d8b42009dfe13c86c7fbb1833750cee1
jungyeon0708 <kimkjy0708@gmail.com>
2025-06-18 10:48:48 +0900

    [학습] 배열(array) 학습

---
+++ assets/script/learn/30-array.js
@@ -0,0 +1,10 @@
+//배열 만들기
+
+const 장바구니 = ["사과", "바나나", "오렌지"];
+console.log(장바구니);
+
+const 장바구니_항목_갯수 = 장바구니.length;
+console.log("장바구니_항목_갯수 = ", 장바구니_항목_갯수);
+
+const numbers = new Array("one", "two", "three");
+console.log(numbers, numbers.length);
